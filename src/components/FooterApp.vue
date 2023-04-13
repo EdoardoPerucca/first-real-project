aaf8cf57b7ab94867cb28ebdffd6442e207aa24f
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-04-13 09:35:35 +0200

    footer completed

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -11,25 +11,29 @@ export default {
             store,
 
             cards: [
-                {
-                    title: 'Contact info',
-                    text: ["Product Support", "Checkout"],
-                },
+
                 {
                     title: 'Information',
-                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis omnis porro harum itaque velit sit deserunt nobis laudantium minima. Nostrum molestiae ipsa architecto odit. Quidem voluptate ducimus dignissimos aliquid minus.',
+                    links: [
+                        'Product Support',
+                        'Checkout',
+                        'Report Abuse',
+                        'Redeem Voucher',
+                        'Order Status',
+                    ]
                 },
                 {
                     title: 'Useful Links',
-                    text: "Design is our playground, while we create an awesome experience, we loke having fun. No animals",
-                },
-                {
-                    title: 'Newsletter',
-                    text: "Design is our playground, while we create an awesome experience, we loke having fun. No animals",
+                    links: [
+                        'Policies & Rules',
+                        'Privacy Policy',
+                        'License Policy',
+                        'My Account',
+                        'Locality',
+                    ]
                 },
 
             ],
-
 
         }
     },
@@ -47,13 +51,52 @@ export default {
 
             <div id="footer-list">
 
-                <div v-for="card in cards" class="footer-single-cards">
-                    <ul>
-                        <li>{{ card.title }}</li>
-                        <li>{{ card.text[""] }}</li>
-                    </ul>
+                <div class="footer-single-cards">
+
+                    <h4>Contact Info</h4>
+                    <div class="contact-info">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>2/45 Tower Street, NewYork USA</p>
+                    </div>
+                    <div class="contact-info">
+                        <i class="fa-sharp fa-regular fa-phone"></i>
+                        <p>Call Us On 08008401010</p>
+                    </div>
+                    <div class="contact-info">
+                        <i class="fa-regular fa-envelope"></i>
+                        <p>Demo@Example.com</p>
+                    </div>
+
                 </div>
 
+                <div v-for="card in cards" class="footer-single-cards">
+                    <strong>
+                        <h4>{{ card.title }}</h4>
+                    </strong>
+                    <ul>
+                        <li v-for="link in card.links">{{ link }}</li>
+                    </ul>
+
+                </div>
+
+                <div class="footer-single-cards">
+
+                    <div id="newsletter">
+                        <h4>Newsletter</h4>
+                        <p>Subscribe to receive inspiration, <br> Ideas & News in your inbox</p>
+                        <input type="text" placeholder="Enter Your Email">
+                        <button>Subscribe</button>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <hr>
+
+            <div id="copyright">
+                <i class="fa-regular fa-copyright"></i>
+                2023 All rights reserved. Developed By TemplateMela
             </div>
 
         </div>
@@ -79,10 +122,69 @@ export default {
 
             .footer-single-cards {
                 display: flex;
+                flex-direction: column;
                 width: 200px;
-                height: 200px;
-                border: 1px solid red;
+
+                h4 {
+                    text-transform: uppercase;
+                    padding-bottom: 1em;
+                }
+
+                .contact-info {
+                    display: flex;
+                    align-items: center;
+                    gap: 10px;
+                    font-size: 13px;
+                    text-transform: capitalize;
+                    color: #c2c3c7;
+                }
+
+                li {
+                    line-height: 2em;
+                    font-size: 13px;
+                    color: #c2c3c7;
+                }
+
+                #newsletter {
+                    font-size: 13px;
+                    line-height: 1.5em;
+
+                    p {
+                        color: #c2c3c7;
+                    }
+
+                    input {
+                        margin-top: 1em;
+                        margin-bottom: 1em;
+                        padding: .8em;
+                        background-color: #1c1c1c;
+                        border: none;
+                    }
+
+                    button {
+                        padding: 1em 2.5em;
+                        text-transform: uppercase;
+                        font-weight: bold;
+                        border: none;
+                    }
+                }
+
             }
+        }
+
+        hr {
+            margin-top: 5em;
+            margin-bottom: 1.5em;
+            color: #c2c3c7;
+        }
+
+        #copyright {
+            text-align: center;
+            padding-bottom: 1em;
+            font-size: 13px;
+            text-transform: capitalize;
+            color: #c2c3c7;
+            font-weight: bold;
         }
     }
 
