7b318c132f16bc7f7d627069b9a79c778e8439b0
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-04-13 10:19:07 +0200

    header completed

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -31,25 +31,51 @@ export default {
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Blog</a></li>
-                <li><a href="#">Pages</a></li>
+                <li><a href="#">Pages</a>
+
+
+                    <div class="dropdown">
+                        <i class="fa-solid fa-chevron-down"></i>
+                        <div class="dropdown-content">
+                            <a href="#">Page 1</a>
+                            <a href="#">Page 2</a>
+                            <a href="#">Page 3</a>
+                            <a href="#">Page 4</a>
+                            <a href="#">Page 5</a>
+                        </div>
+                    </div>
+
+                </li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contacts</a></li>
                 <li><a href="#">Gallery</a></li>
-                <li><a href="#">Shop</a></li>
+                <li><a href="#">Shop</a>
+
+                    <div class="dropdown">
+                        <i class="fa-solid fa-chevron-down"></i>
+                        <div class="dropdown-content">
+                            <a href="#">Shop 1</a>
+                            <a href="#">Shop 2</a>
+                            <a href="#">Shop 3</a>
+                            <a href="#">Shop 4</a>
+                            <a href="#">Shop 5</a>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
 
 
         <div id="nav-right">
             <div id="nav-right-search">
-                <i class="fa-solid fa-magnifying-glass"></i>
+                <a href="#"><img src="../../public/image (23).svg" alt="Search icon"></a>
             </div>
 
             <div id="nav-right-icons">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><img src="../../public/image (9).svg" alt="Search icon"></a>
+                <a href="#"><img src="../../public/image (10).svg" alt="Search icon"></a>
+                <a href="#"><img src="../../public/image (11).svg" alt="Search icon"></a>
+                <a href="#"><img src="../../public/image (12).svg" alt="Search icon"></a>
             </div>
         </div>
 
@@ -62,7 +88,7 @@ export default {
     justify-content: space-between;
     align-items: center;
     height: 50px;
-    padding: 1em;
+    padding: 2em;
 
 
     #nav-left {
@@ -82,7 +108,38 @@ export default {
             a {
                 color: black;
                 font-size: 12px;
+                font-weight: bold;
             }
+        }
+
+        .dropdown {
+            position: relative;
+            display: inline-block;
+            padding-left: .3em;
+            cursor: pointer;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 10px 15px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown-content a:hover {
+            background-color: lightgray
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
         }
     }
 
@@ -90,9 +147,25 @@ export default {
         display: flex;
         gap: 1em;
 
+        #nav-right-search {
+            display: flex;
+            align-items: center;
+            padding-right: 1em;
+            border-right: 1px solid lightgrey;
+
+            img {
+                height: 18px;
+            }
+        }
+
         #nav-right-icons {
             display: flex;
+            align-items: center;
             gap: .5em;
+
+            img {
+                height: 18px;
+            }
 
             a {
                 color: black;
